d9368eb7827c816d74b7e53e3f5e0f1e11773d9d
Студент группы IS-22-9-2 <IS22-9-2@sielom.local>
2024-09-28 12:35:31 +0500

    new_beggining

--- web1/j1.py
+++ web1/j1.py
@@ -9,7 +9,22 @@ def hello():
 def a():
     return render_template('a.html')
 
+@app.route('/rus')
+def rus():
+    return render_template('rus.html')
 
+@app.route('/eng')
+def eng():
+    return render_template('eng.html')
+
+@app.route('/table')
+def table():
+    return render_template('table.html')
+
+
+@app.route('/image')
+def image():
+    return render_template('image.html')
 
 if __name__ == '__main__':
     app.run()
