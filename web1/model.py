3e5e7b4753058713bb5a87c7fb824a709c87311d
Me11ad <vlad505200483@gmail.com>
2024-10-18 17:42:18 +0500

    edfoiqhyegbqeiprh

--- web1/model.py
+++ web1/model.py
@@ -6,19 +6,23 @@ app = Flask(__name__)
 app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
 db = SQLAlchemy(app)
 
-class Character(db.Model):
+class character(db.Model):
     character_id = db.Column('character_id',db.Integer(), primary_key=True)
     name = db.Column(db.String(50))
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
-    class_id = db.Column(db.Integer, db.ForeignKey("class.class_id"), nullable=False)
+    class_id = db.Column(db.Integer, db.ForeignKey("classe.class_id"), nullable=False)
     race_id = db.Column(db.Integer, db.ForeignKey('race.race_id'), nullable=False)
     background_id = db.Column(db.Integer, db.ForeignKey('background.background_id'), nullable=False)
     
+    user = db.relationship("user")
+    classe = db.relationship("classe")
+    race = db.relationship("race")
+    background = db.relationship("background")
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
-class User(db.Model):
+class user(db.Model):
     user_id = db.Column('user_id', db.Integer, primary_key=True)
     username = db.Column(db.String(50))
     password = db.Column(db.String(50))
@@ -27,62 +31,71 @@ class User(db.Model):
         self.username = username
         self.password = password
 
-class Classes(db.Model):
-    class_is = db.Column(db.Integer)
+class classe(db.Model):
+    class_id = db.Column('class_id', db.Integer, primary_key=True)
     class_name = db.Column(db.String(50))
 
-class Class_Abibilities(db.Model):
-    class_id = db.Column(db.Integer, db.ForeignKey('Classes.class_id'))
+class class_Abibilities(db.Model):
+    class_Abibilities_id = db.Column('class_id', db.Integer, primary_key=True)
+    class_id = db.Column(db.Integer, db.ForeignKey('classe.class_id'))
+    classe = db.relationship("classe")
     ability_name = db.Column(db.String(50))
     ability_description = db.Column(db.String(50))
 
-class Class_Attributes(db.Model):
-    class_id = db.Column(db.Integer, db.ForeignKey('Classes.class_id'))
+class class_Attributes(db.Model):
+    class_id = db.Column(db.Integer, db.ForeignKey('classe.class_id'))
+    classe = db.relationship("class")
     ability_name = db.Column(db.String(50))
     ability_value = db.Column(db.Integer)
     
-class Class_Skills(db.model):
-    class_id = db.Column(db.Integer, db.ForeignKey('Classes.class_id'))
-    skill_id = db.Column(db.Integer, db.ForeignKey('Skills.skill_id'))
+class class_Skills(db.model):
+    class_id = db.Column(db.Integer, db.ForeignKey('classe.class_id'))
+    classe = db.relationship("classe")
+    skill_id = db.Column(db.Integer, db.ForeignKey('skill.skill_id'))
+    skill = db.relationship("skill")
 
-class Skills(db.Model):
+class skill(db.Model):
     skill_id = db.Column('skill_id', db.Integer, primary_key=True)
     skill_name = db.Column(db.String(50))
 
-class Race(db.Model):
+class race(db.Model):
     race_id = db.Column('race_id', db.Integer, primary_key=True)
     race_name = db.Column(db.String(50))
 
-class Race_Attributes(db.Model):
-    race_id = db.Column(db.Integer, db.ForeignKey('Race.race_id'))
+class race_Attributes(db.Model):
+    race_id = db.Column(db.Integer, db.ForeignKey('race.race_id'))
+    race = db.relationship("race")
     attribute_name = db.Column(db.String(50))
     attribute_value = db.Column(db.Integer)
 
-class Weapon(db.Model):
+class weapon(db.Model):
     weapon_id = db.Column('weapon_id', db.Integer, primary_key=True)
     weapon_name = db.Column(db.String(50))
     damage = db.Column(db.Integer)
 
-class Weapon_properties(db.Model):
+class weapon_properties(db.Model):
         property_id = db.Column('property_id', db.Integer, primary_key=True)
         property_name = db.Column(db.String(50))
 
-class Weapon_weaponProperties(db.Model):
-    weapon_id = db.Column(db.Integer, db.ForeignKey('Weapon.weapon_id'))
-    property_id = db.Column(db.Integer, db.ForeignKey('Weapon_properties.property_id'))
+class weapon_weaponProperties(db.Model):
+    weapon_id = db.Column(db.Integer, db.ForeignKey('weapon.weapon_id'))
+    weapon = db.relationship("weapon")
+    property_id = db.Column(db.Integer, db.ForeignKey('weapon_properties.property_id'))
+    weapon_properties = db.relationship("weapon_properties")
     
-class Armor(db.Model):
+class armor(db.Model):
         armor_id = db.Column('armor_id', db.Integer, primary_key=True)
         armor_name = db.Column(db.String(50))
         defense_value = db.Column(db.Integer)
         
-class Background(db.Model):
+class background(db.Model):
     background_id = db.Column('background_id', db.Integer, primary_key=True)
     background_name = db.Column(db.String(50))
     
-class Background_Skills(db.Model):
-    background_id = db.Column(db.Integer, db.ForeignKey('Background.background_id'))
-    skill_id = db.Column(db.Integer, db.ForeignKey('Skills.skill_id'))
+class background_Skills(db.Model):
+    background_id = db.Column(db.Integer, db.ForeignKey('background.background_id'))
+    background = db.relationship("background")
+    skill_id = db.Column(db.Integer, db.ForeignKey('skill.skill_id'))
     
 
     
@@ -90,10 +103,10 @@ class Background_Skills(db.Model):
     
     
 
-with app.app_context():
-    db.create_all()
-    users = User('fvpoijnqerp', '4321')
-    db.session.add(users)
-    db.session.commit()
+# with app.app_context():
+#     db.create_all()
+#     users = User('fvpoijnqerp', '4321')
+#     db.session.add(users)
+#     db.session.commit()
     
     
